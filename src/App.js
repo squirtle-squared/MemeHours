424523561c67e97ea713db516262274abedd1605
Kadir Gundogdu <kadirgund@gmail.com>
2020-09-29 01:43:56 -0400

    fetching generated meme

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import React, { useState, useEffect, useRef } from 'react';
 import socketIOClient from "socket.io-client";
+const temps = require('./templates.js');
 const ENDPOINT = 'http://localhost:3001';
 
 export default function App() {
 	const [texts, setTexts] = useState(['', '', '', '', '']);
-	const [didMount, setDidMount] = useState(false);
-	const [templates, setTemplates] = useState([{
-		box_count: 2,
-		height: 1200,
-		id: "181913649",
-		name: "Drake Hotline Bling",
-		url: "https://i.imgflip.com/30b1gx.jpg",
-		width: 1200
-	}, {
-		box_count: 3,
-		height: 800,
-		id: "112126428",
-		name: "Distracted Boyfriend",
-		url: "https://i.imgflip.com/1ur9b0.jpg",
-		width: 1200
-	}]);
+	// const [didMount, setDidMount] = useState(false);
+	const [templates, setTemplates] = useState(temps);
 	const [currentMeme, setCurrentMeme] = useState(templates[Math.floor(Math.random() * templates.length)])
 	const imgRef = useRef(null);
-
-
-	// handleClick = (e) => {
-	// 	e.preventDefault();
-	// };
 
 	const handleChange = (e) => {
 		const inputFields = [...texts];
@@ -56,36 +38,35 @@ export default function App() {
 		fetch("https://api.imgflip.com/caption_image", requestOptions)
 			.then(res => res.json())
 			.then(res => {
-				console.log(res)
 				imgRef.current.src = res.data.url
 			})
 			.catch(error => console.log('error', error))
 	}
 
 	useEffect(() => {
-		if (!didMount) {
-			setDidMount(true);
-			fetch('https://api.imgflip.com/get_memes')
-				.then(res => res.json())
-				.then(res => console.log(res.data.memes))
-			// .then(res => setTemplates(res))
-		}
+		// if (!didMount) {
+		// 	setDidMount(true);
+		// 	fetch('https://api.imgflip.com/get_memes')
+		// 		.then(res => res.json())
+		// 		.then(res => console.log(res.data.memes))
+		// 	// .then(res => setTemplates(res))
+		// }
 		const socket = socketIOClient(ENDPOINT);
 	}, []);
 
 
 	const textBoxes = [];
 	for (let i = 0; i < currentMeme.box_count; i++) {
-		console.log(currentMeme.box_count)
 		textBoxes.push(<input type='text' key={`text-${i}`} id={i} value={texts[i]} onChange={handleChange} />)
 	}
-
-	console.log(texts)
 
 	return (
 		<div>
 			<p>HELLO MEME HOURS!!!</p>
+			<span>{currentMeme.name}</span>
+			<br />
 			<img ref={imgRef} src={currentMeme.url} />
+			<br />
 			{textBoxes}
 			<button onClick={handleClick}>Generate</button>
 		</div>
