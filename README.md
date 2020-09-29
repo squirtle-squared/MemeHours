6640bc9bb946acf550bd344b3157e0dbca696d94
Heather Friedman <hfried25@gmail.com>
2020-09-28 21:54:19 -0400

    file structure and webpack

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MEME HOURS
+
+First:
+`npm run build`
+
+Then:
+`npm start` for hot module reloading!
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['react-hot-loader/patch', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+        include: [path.resolve(__dirname + '/src')],
+        loader: 'url-loader?limit=50000&name=[name].[ext]',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    historyApiFallback: true,
+  },
+};
