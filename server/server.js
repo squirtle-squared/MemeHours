183c6059f46acd701f4be77cfe4440a4b6994968
Heather Friedman <hfried25@gmail.com>
2020-09-29 15:49:18 -0400

    login and create page frontend

--- server/server.js
+++ server/server.js
@@ -9,17 +9,15 @@ const server = app.listen(port, () => console.log(`server listening on ${port}.`
 
 app.use('/', express.static(path.join(__dirname, '../dist')));
 app.get('*', (_request, response) =>
-	response.sendFile(path.resolve(`${__dirname}/../dist/index.html`)),
+  response.sendFile(path.resolve(`${__dirname}/../dist/index.html`)),
 );
 
 const io = socket(server);
 
-io.on('connection', (socket) => {
-	console.log('new client connected');
+io.on('connection', socket => {
+  console.log('new client connected');
 
-	io.on('disconnect', (socket) => {
-		console.log('a user disconnected');
-	})
+  io.on('disconnect', socket => {
+    console.log('a user disconnected');
+  });
 });
-
-
