e24ae7940637473bc70683ec0a3700d174687bb9
Kadir Gundogdu <kadirgund@gmail.com>
2020-09-29 20:25:00 -0400

    first sockets

--- server/server.js
+++ server/server.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const socket = require('socket.io');
+const socketio = require('socket.io');
 
 const port = process.env.PORT || 3001;
-
-const server = app.listen(port, () => console.log(`server listening on ${port}.`));
 
 app.use('/', express.static(path.join(__dirname, '../dist')));
 app.get('*', (_request, response) =>
   response.sendFile(path.resolve(`${__dirname}/../dist/index.html`)),
 );
-
-const io = socket(server);
+const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
+const io = socketio(server);
 
 io.on('connection', socket => {
   console.log('new client connected');
 
-  io.on('disconnect', socket => {
+  socket.emit('messageFromServer', { data: 'message from server' });
+  socket.on('messageFromClient', msg => {
+    console.log(msg);
+  });
+
+  socket.on('disconnect', socket => {
     console.log('a user disconnected');
   });
 });
--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 import StageOne from './StageOne';
 import Home from './Home';
 import { Switch, Route } from 'react-router-dom';
+import io from 'socket.io-client';
 
 export default function App() {
+  const socket = io.connect('http://localhost:3001');
+  socket.on('messageFromServer', msg => {
+    console.log(msg);
+    socket.emit('messageFromClient', { data: 'message from client' });
+  });
   return (
     <div>
       <Switch>
