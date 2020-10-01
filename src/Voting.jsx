75b51acf35da91c0e5a133d154107f58a6e9038e
Rebecca Miller <beemills@gmail.com>
2020-09-30 22:32:45 -0400

    memes rendering during voting

--- src/Voting.jsx
+++ src/Voting.jsx
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import Candidates from './Candidates';
 
 export default function Voting({ socket }) {
+  const [candidates, setCandidates] = useState([]);
   const history = useHistory();
+  let memes;
+
+  socket.emit('getCandidates');
 
   useEffect(() => {
+    socket.on('memeCandidates', memes => {
+      setCandidates(memes);
+    });
     socket.on('winner', () => {
       history.push('/winner');
     });
   }, []);
 
-  return <div>hello world! let's vote</div>;
+  return <div>{candidates.length && candidates.map(meme => <img src={meme.memeUrl} />)}</div>;
 }
